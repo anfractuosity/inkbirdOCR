6bf2e9b8a08cc6c5c5b30c13e24b039c12922cfd
anfractuosity <anfractuosity@anfractuosity.com>
2017-05-30 16:32:52 +0100

    two scripts

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
 # inkbirdOCR
+
 Using SSOCR to OCR an Inkbird for graphing fermentation
+
+I've created two scripts:
+
+# log-closest-to-target.sh
+
+Finds the OCR'd value which is closest to the target value you specify in the script.
+
+# log-most-common.sh
+
+Finds the OCR'd value which appears the most common. 
 
 See https://www.anfractuosity.com/projects/fermentation-temperature-control-with-inkbird-308/ for more details on how the hardware is setup.
---
+++ log-most-common.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+# The Inbird target temperature was set to 20C for my particular yeast (wlp023)
+TARGET=200
+
+while true; do
+
+	# Take photo using current date	
+	DATE=$(date +"%Y-%m-%d-%H%M")
+	raspistill -vf -hf -o $DATE.jpg
+
+	ARRAY=()
+	X=0
+
+	# Try different thresholds
+	for I in 90 80 50 40 20 10; do
+
+		# Ensure there is at least PIX pixels
+		for PIX in 50 20 5; do
+			
+			# Shear image by different amounts 
+			for SHEAR in 65 10; do
+
+				Z=$(../../ssocr/ssocr -d3 -i$PIX rotate 359 crop 624 821 1230 550 shear $SHEAR -t$I -f white ${DATE}.jpg -o dump.jpg)
+	
+				# Ensure exit code was 0, meaning OCR detected numbers of 3 digits
+				if [ $? -eq 0 ] && [ "${Z: -1}" != "-" ]; then
+					echo $Z
+					# Store number from OCR as integer, convert b to 6 
+					# (as ssocr sometimes thinks 6 is B in hex)
+					NUMI=$(echo "$(echo $Z | sed "s/b/6/g")" | bc)	
+					((ARRAY[$NUMI]++))
+					X=1
+				fi
+			done
+		done 
+	done
+
+	if [ $X -eq 0 ]; then
+		# Failed to detect display correctly	
+		echo ${DATE} >> bad
+	else
+		LOW=0
+		LVAL=-1
+
+		# Find best value out of possible candidate OCRd values
+		for var in "${!ARRAY[@]}";
+		do	
+			# Check if this number is the smallest different from target	
+			if [ ${ARRAY[$var]} -gt $LOW ]; then
+				LOW=${ARRAY[$var]}
+				LVAL=$var
+			fi
+		done
+		
+		# Convert number to decimal
+		NUM=$(echo "scale = 2; $LVAL / 10" | bc)
+
+		# Write number to CSV
+		echo "${DATE},$NUM" >> temps
+		
+		rm ${DATE}.jpg
+	fi
+
+	sleep 60
+done
